c7ac57ce4c165e125a78aca6897b7676d70375c3
martin <martin.lange@csa-germany.de>
2022-04-01 21:03:30 +0200

    editorial: add comments

--- utils.js
+++ utils.js
@@ -13,14 +13,15 @@ const tryCatch = async(req, res, func) => {
 /// Replace single quotes with double ones to escape then before inserting string as value
 const replaceQuotes = (value) => value && (typeof value === 'string') ? value.replace(/'/g, '\'\'') : value;
 
-
 /// Returns the query to a single column as part of a complete UPDATE command.
 /// If column value is undefined, it returns an empty string.
 /// Handles the correct concatenation of the comma between the single column expressions and the framing of the value with quotes
 let firstIsHandled;
 const getParamQuery = (name, value, first = false, valueQuotes = true) => {
     if (first) firstIsHandled = false;
+    // if not applicable omit quotes around values 
     const q = valueQuotes ? `'` : ``;
+    // only if surrounding quotes are required, 'escape' them with "''"  
     if (valueQuotes) value = replaceQuotes(value);
     const res = (value && firstIsHandled ? ', ' : '') + (value ? `${name} = ${q}${value}${q}` : ``);
     firstIsHandled = true;

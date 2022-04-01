39a21e16d79c6210ba2583df1345ebd2b57db798
martin <martin.lange@csa-germany.de>
2022-04-01 10:32:53 +0200

    add error handling and logging of route requests, add CORS

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 const { StatusCodes } = require("http-status-codes");
 const express = require("express");
+var cors = require('cors');
 const app = express();
+
 const usersRouter = require("./routes/users");
 const widgetsRouter = require("./routes/widgets");
 const screensRouter = require("./routes/screens");
+
+app.use(cors());
+
+// log every request
+app.use((req, res, next) => {
+    console.log('URL:', req.originalUrl);
+    next();
+});
 
 // Routes ------
 app.use('/users', usersRouter);
 app.use('/widgets', widgetsRouter);
 app.use('/screens', screensRouter);
 
-app.use((req, res) => {
-    res.status(StatusCodes.NOT_FOUND).send("Resource not found");
-})
+// Error handling -----
+app.get('*', function(req, res, next) {
+    const error = new Error(
+        `${req.ip} tried to access ${req.originalUrl}`,
+    );
+    error.statusCode = StatusCodes.MOVED_PERMANENTLY;
+    next(error);
+});
 
+app.use((error, req, res, next) => {
+    if (!error.statusCode)
+        error.statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+
+    // TODO: implement '404' redirect page
+    // if (error.statusCode === StatusCodes.MOVED_PERMANENTLY) {
+    //     return res.status(StatusCodes.MOVED_PERMANENTLY).redirect('/not-found');
+    // }
+
+    return res.status(error.statusCode).json({ error: error.toString() });
+});
 
 const PORT = 3010;
 app.listen(PORT, () => {

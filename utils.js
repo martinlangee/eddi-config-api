247f3869839f6f8140e2aff6f52cbbe341778fab
martin <martin.lange@csa-germany.de>
2022-04-08 13:41:39 +0200

    server displays local ip adresses on start up

--- utils.js
+++ utils.js
@@ -44,8 +44,8 @@ const getLocalIpAddresses = () => {
 
     for (const name of Object.keys(nets)) {
         for (const net of nets[name]) {
-            // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-            if (net.family === 'IPv4' && !net.internal) {
+            // Skip over internal (i.e. 127.0.0.1) addresses
+            if (!net.internal) {
                 if (!results[name]) {
                     results[name] = [];
                 }
